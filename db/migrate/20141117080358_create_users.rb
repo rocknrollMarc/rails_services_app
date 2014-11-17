bc2ba5dcda126597473d2e4045e59cd108da9179
Marc <marc.bluemner@gmail.com>
2014-11-17 09:04:30 +0100

    gems setup

---
+++ db/migrate/20141117080358_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :bio
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+email "MyString"
+bio "MyString"
+created_at "2014-11-17 09:03:58"
+updated_at "2014-11-17 09:03:58"
+  end
+
+end
